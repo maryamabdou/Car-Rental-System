19368c83ee9ec0c1c7031174dd67b0d923baf763
Youmna_Elmezayen <68197709+Youmna-Elmezayen@users.noreply.github.com>
2022-12-30 19:27:20 +0200

    Update car_register.php

--- car_register.php
+++ car_register.php
@@ -5,26 +5,19 @@
     $plate_id = $_POST['plate_id'];
     $year = $_POST['year'];
     $office_loc = $_POST['location'];
-    if($model == "" || $plate_id=="" || $year=="" || $office_loc=="")
-    {
-        echo '<script>alert("Please re-enter")</script>';
-        $script = "<script>
-        location = 'admin.php';</script>";
-        echo $script;
-    }
-    else{
         $link = mysqli_connect('localhost', 'root', '') or die(mysqli_error());
         mysqli_select_db($link ,'car_rental_system') or die(mysqli_error());
         $sql = " INSERT INTO car (plate_id , model , `year`, office_loc)  VALUES ('$plate_id', '$model', '$year', '$office_loc') ";
         mysqli_query($link, $sql);
         mysqli_close($link);
-    }
+	 
+	 $script = "<script>
+	location = 'admin.php';</script>";
+	echo $script;
 	   
 }
  
-		   $script = "<script>
-		   location = 'admin.php';</script>";
-		   echo $script;
+		   
 
 ?>
 </html>
